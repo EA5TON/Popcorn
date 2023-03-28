f64f383205362422e1b8f86f10cb6ffe4f65b8b7
EA5TON <Lexa5Sk@rambler.ru>
2023-03-28 22:02:41 +0300

    Start.
    31

---
+++ Popcorn/Border.cpp
@@ -0,0 +1,94 @@
+﻿#include "Border.h"
+
+// AsBorder.
+//------------------------------------------------------------------------------------------------------------------------
+AsBorder::AsBorder ()
+{
+}
+//------------------------------------------------------------------------------------------------------------------------
+void AsBorder::Draw (HDC hdc, RECT &paint_area)
+{// Отрисовка рамки.
+
+    int i;
+
+    // 1. Линия слева.
+    for (i = 0; i < 50; i++)
+        Draw_Element (hdc, 2, 1 + i * 4, false);
+
+    // 2. Линия справа.
+    for (i = 0; i < 50; i++)
+        Draw_Element (hdc, 201, 1 + i * 4, false);
+
+    // 3. Линия сверху.
+    for (i = 0; i < 50; i++)
+        Draw_Element (hdc, 3 + i * 4, 0, true);
+}
+//------------------------------------------------------------------------------------------------------------------------
+bool AsBorder::Check_Hit (double next_x_pos, double next_y_pos, ABall *ball)
+{
+    bool got_hit = false;
+
+    // Корректируем позицию при отражении от рамки.
+    if (next_x_pos - ball-> Radius < AsConfig::Border_X_Offset)
+    {
+        got_hit = true;
+        ball-> Reflect (false);
+    }
+
+    if (next_y_pos - ball-> Radius < AsConfig::Border_Y_Offset)
+    {
+        got_hit = true;
+        ball-> Reflect (true);
+    }
+
+    if (next_x_pos + ball-> Radius > AsConfig::Max_X_Pos)
+    {
+        got_hit = true;
+        ball-> Reflect (false);
+    }
+
+    if (next_y_pos + ball-> Radius > AsConfig::Max_Y_Pos)
+    {
+        if (AsConfig::Level_Has_Floor)
+        {
+            got_hit = true;
+            ball-> Reflect (true);
+        }
+        else
+        {
+            if (next_y_pos + ball-> Radius > AsConfig::Max_Y_Pos + ball-> Radius * 4.0) // Проверяем max_x_pos мяча для того, чтобы он мог улететь ниже пола. 
+                ball-> Set_State (EBS_Lost, next_x_pos);
+        }
+    }
+    return got_hit;
+}
+//------------------------------------------------------------------------------------------------------------------------
+void AsBorder::Draw_Element (HDC hdc, int x, int y, bool top_border)
+{// Отрисвока элемента рамки.
+
+    // Основная линия.
+    AsConfig::Whiteblue_Color.Select (hdc);
+
+    if (top_border)
+        Rectangle (hdc, x * AsConfig::Global_Scale, (y + 1) * AsConfig::Global_Scale, (x + 4) * AsConfig::Global_Scale - 1, (y + 4) * AsConfig::Global_Scale - 1);
+    else
+        Rectangle (hdc, (x + 1) * AsConfig::Global_Scale, y * AsConfig::Global_Scale, (x + 4) * AsConfig::Global_Scale - 1, (y + 4) * AsConfig::Global_Scale - 1);
+
+    // Белая кайма.
+    AsConfig::White_Color.Select (hdc);
+
+
+    if (top_border)
+        Rectangle (hdc, x * AsConfig::Global_Scale, y * AsConfig::Global_Scale, (x + 4) * AsConfig::Global_Scale - 1, (y + 1) * AsConfig::Global_Scale);
+    else
+        Rectangle (hdc, x * AsConfig::Global_Scale, y * AsConfig::Global_Scale, (x + 1) * AsConfig::Global_Scale - 1, (y + 4) * AsConfig::Global_Scale);
+
+    // Перфорация.
+    AsConfig::BG_Color.Select (hdc);
+
+    if (top_border)
+        Rectangle (hdc, (x + 2) * AsConfig::Global_Scale, (y + 2) * AsConfig::Global_Scale, (x + 3) * AsConfig::Global_Scale - 1, (y + 3) * AsConfig::Global_Scale - 1);
+    else
+        Rectangle (hdc, (x + 2) * AsConfig::Global_Scale, (y + 1) * AsConfig::Global_Scale, (x + 3) * AsConfig::Global_Scale - 1, (y + 2) * AsConfig::Global_Scale - 1);
+}
+//------------------------------------------------------------------------------------------------------------------------
---
+++ Popcorn/Config.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include <Windows.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+//------------------------------------------------------------------------------------------------------------------------
+class AColor
+{
+public:
+    AColor ();
+    AColor (unsigned char r, unsigned char g, unsigned char b);
+    AColor (const AColor &color, int pen_size);
+
+    int Get_RGB () const;
+    void Select (HDC hdc) const;
+    void Select_Pen (HDC hdc) const;
+    HBRUSH Get_Brush () const;
+
+    unsigned char R, G, B;
+private:
+    HPEN Pen;
+    HBRUSH Brush;
+};
+//------------------------------------------------------------------------------------------------------------------------
+class AsConfig
+{
+public:
+    static int Rand (int range);
+    static void Round_Rect (HDC hdc, RECT &rect, int corner_radius = 2);
+    static void Throw ();
+
+    static bool Level_Has_Floor;
+    static int Current_Timer_Tick;
+    static const AColor BG_Color, Purpule_Color, Whiteblue_Color, White_Color, Letter_Color;
+    static HWND hwnd;
+
+    static const double Moving_Step_Size;
+    static const double PI_2;
+
+    static const int Global_Scale = 3;
+    static const int Border_X_Offset = 6;
+    static const int Border_Y_Offset = 4;
+    static const int Level_Width = 12; // Ширина уровня в ячейках.
+    static const int Level_Height = 14; // Высота уровня в ячейках.
+    static const int Level_X_Offset = 8;
+    static const int Level_Y_Offset = 6;
+    static const int Cell_Width = 16;
+    static const int Cell_Height = 8;
+    static const int Brick_Width = 15;
+    static const int Brick_Height = 7;
+    static const int Max_X_Pos = Level_X_Offset + Cell_Width * Level_Width;
+    static const int Max_Y_Pos = 199;
+    static const int Platform_Y_Pos = 185;
+    static const int FPS = 30;
+    static const int Max_Active_Bricks_Count = 10;
+    static const int Hits_Per_Letter = 1; // Вероятность выпадения буквы 1.0 / Hits_Per_Letter.
+    static const int Max_Falling_Letters_Count = 10;
+};
+//------------------------------------------------------------------------------------------------------------------------
